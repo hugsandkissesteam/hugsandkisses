e09c74eff0a132b536fb6f51ebc68caf7d38f879
hugsandkissesteam <151214419+hugsandkissesteam@users.noreply.github.com>
2023-11-17 23:14:21 +0530

    initial commit

---
+++ html - Copy (2) - Copy/js/owl.carousel.js
@@ -0,0 +1,6 @@
+$('.owl-carousel').owlCarousel({
+    // Your carousel settings go here
+    // ...
+    nav: true, // Enable navigation
+    navText: ["<button class='custom-prev-button'>Previous</button>", "<button class='custom-next-button'>Next</button>"]
+});
